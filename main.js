85cbfbb4c1655d23d1e6a9e6398f4447f8e76f79
Itish-Raj-Singh <94170586+Itish-Raj-Singh@users.noreply.github.com>
2022-03-07 15:19:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-function startClassification(){
-   navigator.mediaDevices.getUserMedia({audio:true});
-   classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/9-hh6ypJQ/model.json')
+function startClassifictaion() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/9-hh6ypJQ/model.json',modelReady);
 }
-function modelReady(){
-    classifier.classify(gotResults);
-}
-function gotResults(results , error){
-    if(error){
-        console.error(error);
-    }
+function modelReady() {
+    classifier.classify(gotResult);
 }
